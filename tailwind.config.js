e1002a676bad5762d3fe076a87775f302168242c
MattSeb15 <mateosebastian159@gmail.com>
2024-08-23 22:29:32 -0500

    feat: Add custom gray colors to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'custom-gray': {
+					1: '#21252b',
+					2: '#282c34',
+					3: '#333842',
+				},
+			},
+		},
 	},
 	plugins: [require('daisyui')],
 	daisyui: {
