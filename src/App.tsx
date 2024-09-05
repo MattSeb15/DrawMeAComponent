b67aac03feb50049f31585905a10a384c2f35438
MattSeb15 <mateosebastian159@gmail.com>
2024-09-05 02:02:07 -0500

    chore: Update CategoryPanel component to CategoryItem and refactor usage in App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import { useEffect, useState } from 'react'
 import './App.css'
 import { Category, Component } from './interfaces/panels/draw-me-panel'
 import DrawMePanel from './components/panels/draw-me-panel'
-import CategoryPanel from './components/panels/category-panel'
+import CategoryItem, {
+	ComponentPanel,
+} from './components/panels/category-panel'
 
 function App() {
 	const [components, setComponents] = useState<Component[]>(() => {
@@ -23,6 +25,10 @@ function App() {
 			  ]
 	})
 
+	const [selectedCategory, setSelectedCategory] = useState<Category | null>(
+		null
+	)
+
 	useEffect(() => {
 		localStorage.setItem('components', JSON.stringify(components))
 	}, [components])
@@ -33,10 +39,47 @@ function App() {
 
 	return (
 		<div className='grid grid-cols-5 grid-rows-5 w-full h-full bg-custom-gray-3'>
-			<div className='col-span-4 row-span-4'>
+			<div className='col-span-4 row-span-4 relative'>
 				<div className='w-full h-full overflow-scroll'>
-					<div className='w-[5000px] h-[5000px] static paper '></div>
+					<div className='w-[5000px] h-[5000px] paper'></div>
 				</div>
+				{selectedCategory && (
+					<>
+						<div className='w-fit h-[85%] max-h-fit bg-custom-gray-3 p-2 drop-shadow-lg overflow-auto border-custom-gray-2 border-2 rounded-lg absolute bottom-7 left-2'>
+							<div className='flex flex-col gap-2 relative'>
+								{selectedCategory ? (
+									components.filter(
+										component => component.categoryId === selectedCategory.id
+									).length > 0 ? (
+										components
+											.filter(
+												component =>
+													component.categoryId === selectedCategory.id
+											)
+											.map(component => (
+												<ComponentPanel
+													key={component.name}
+													component={component}
+												/>
+											))
+									) : (
+										<div className='px-3'>
+											<span className='icon-[pixelarticons--mood-sad] size-10'></span>
+											<p className='text-sm text-center font-bold'>
+												No components
+												<br />
+												in this category
+											</p>
+										</div>
+									)
+								) : null}
+							</div>
+						</div>
+						<div className='text-start text-sm font-bold absolute top-5 left-2 bg-custom-gray-3 p-2 drop-shadow-lg border-custom-gray-2 border-2 rounded-lg truncate max-w-fit w-72'>
+							{selectedCategory.name}
+						</div>
+					</>
+				)}
 			</div>
 			<div className='row-span-5 col-start-5 bg-custom-gray-1'>
 				<div className='flex flex-col w-full h-full items-center'>
@@ -54,13 +97,20 @@ function App() {
 					/>
 				</div>
 			</div>
-			<div className='col-span-4 row-start-5 bg-custom-gray-2'>
-				<div className='flex w-full h-full gap-4 items-center p-2'>
-					{categories.map(category => (
-						<CategoryPanel
+			<div className='col-span-4 row-start-5 bg-custom-gray-2 relative'>
+				<div className='flex w-full h-full gap-4 items-center p-2 overflow-auto'>
+					{categories.map((category, i) => (
+						<CategoryItem
 							key={category.id}
+							className={i === 0 ? 'ml-5' : ''}
 							category={category}
+							selected={selectedCategory?.id === category.id}
 							components={components.filter(c => c.categoryId === category.id)}
+							onSelectCategory={category => {
+								if (selectedCategory?.id === category.id)
+									setSelectedCategory(null)
+								else setSelectedCategory(category)
+							}}
 						/>
 					))}
 				</div>
--- src/components/panels/category-panel.tsx
+++ src/components/panels/category-panel.tsx
@@ -1,40 +1,35 @@
 import { Category, Component } from '../../interfaces/panels/draw-me-panel'
 
-const CategoryPanel: React.FC<{
+const CategoryItem: React.FC<{
+	className?: string
 	category: Category
 	components: Component[]
-}> = ({ category, components }) => {
-	if (components.length === 0) return null
+	selected: boolean
+	onSelectCategory: (category: Category) => void
+}> = ({ category, components, onSelectCategory, selected, className }) => {
 	return (
-		<div className='indicator w-fit h-fit'>
-			<span className='indicator-item badge indicator-bottom badge-secondary text-white font-bold'>
+		<div
+			className={`indicator w-fit h-fit ` + className}
+			onClick={() => onSelectCategory(category)}>
+			<span className='indicator-item badge indicator-start indicator-bottom bg-custom-gray-3 border border-custom-gray-1 text-white font-bold'>
 				{components.length}
 			</span>
-			<div className='dropdown dropdown-top'>
-				<div
-					tabIndex={0}
-					role='button'
-					className='btn m-1 bg-custom-gray-1 hover:bg-custom-gray-3 border-none'>
-					{category.name}
-				</div>
-				<div
-					tabIndex={0}
-					className='dropdown-content z-[1] max-w-fit w-[500px] h-fit bg-custom-gray-3 p-2 shadow overflow-auto border-custom-gray-2 border-2 rounded-lg'>
-					<div className='flex gap-2'>
-						{components.map(component => (
-							<ComponentPanel
-								key={component.name}
-								component={component}
-							/>
-						))}
-					</div>
-				</div>
+			<div
+				className={
+					'btn m-1 border-none' +
+					(selected
+						? ' bg-blue-700 hover:bg-blue-800'
+						: ' bg-custom-gray-1 hover:bg-custom-gray-3')
+				}>
+				<p className='w-36 truncate'>{category.name}</p>
 			</div>
 		</div>
 	)
 }
 
-const ComponentPanel: React.FC<{ component: Component }> = ({ component }) => {
+export const ComponentPanel: React.FC<{ component: Component }> = ({
+	component,
+}) => {
 	return (
 		<div className='flex-none size-32 bg-custom-gray-2 rounded-lg relative'>
 			<div className='absolute w-full top-1 px-1 flex justify-center'>
@@ -51,4 +46,4 @@ const ComponentPanel: React.FC<{ component: Component }> = ({ component }) => {
 	)
 }
 
-export default CategoryPanel
+export default CategoryItem
